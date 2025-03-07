4b0de47e0b0bbde744fc4302a7c8858b512e8386
Jay Cee Mandap <jhaycee34@yahoo.com>
2025-03-07 16:35:54 +0800

    Add Head title

--- resources/js/pages/App.vue
+++ resources/js/pages/App.vue
@@ -1,10 +1,12 @@
 <script setup>
+import { Head } from '@inertiajs/vue3';
 import Hero from './Hero.vue';
 import AboutUs from './AboutUs.vue';
 import AppointmentForm from './AppointmentForm.vue';
 </script>
 
 <template>
+    <Head title="Home" />
     <div class="font-open-sans">
         <Hero />
         <AboutUs />
